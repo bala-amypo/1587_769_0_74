573c62bced75c9e90a8c580e0f278cd7fce089ab
bala-amypo <bala@amypo.in>
2025-12-27 15:27:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserDTO.java
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,27 +1,13 @@
 package com.example.demo.dto;
 
-import com.example.demo.entity.User;
-import lombok.*;
-import java.time.Instant;
+import com.example.demo.entity.Role;
+import lombok.Data;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data
 public class UserDTO {
-    private Long id;           // 
-    private String fullName;    // 
-    private String email;       // 
-    private User.Role role;     // 
-    private Instant createdAt;  // 
 
-    // Constructor to map from User Entity
-    public UserDTO(User user) {
-        this.id = user.getId();
-        this.fullName = user.getFullName();
-        this.email = user.getEmail();
-        this.role = user.getRole();
-        this.createdAt = user.getCreatedAt();
-    }
+    private Long id;
+    private String username;
+    private String email;
+    private Role role;
 }
